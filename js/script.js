745e51d5ad1ef1362dc8b0a50b9bd49f9f142f7f
emilianogastaldo <emiliano0303@gmail.com>
2024-01-29 15:37:30 +0100

    genero una sola email

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-console.log('ok')
-console.log(Vue)
-console.log(axios)
 
 const { createApp } = Vue;
+
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 const app = createApp({
     name: 'Email List',
     data: () => ({
-        prova: 'Prova vue'
+        prova: 'Prova vue',
+        emailList: []
     }),
+    created() {
+        axios.get(endpoint).then((res) => {
+            const email = res.data;
+            this.emailList.push(email);
+        })
+        console.log('lista di email', this.emailList);
+
+    }
 });
 
 app.mount('#root');
