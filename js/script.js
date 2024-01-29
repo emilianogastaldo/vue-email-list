661f52dfb7697ba849bd0ed6a0bbd964c72c3bba
emilianogastaldo <emiliano0303@gmail.com>
2024-01-29 15:20:41 +0100

    created and mounted vue app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 console.log('ok')
 console.log(Vue)
 console.log(axios)
+
+const { createApp } = Vue;
+
+const app = createApp({
+    name: 'Email List',
+    data: () => ({
+        prova: 'Prova vue'
+    }),
+});
+
+app.mount('#root');
